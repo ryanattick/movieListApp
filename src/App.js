e5467aa03c7ca60803656fe6d0affd3816e8f2e6
Elizabeth VanLente <lizvanlente@Elizabeths-MacBook-Air.local>
2017-08-02 09:24:45 -0700

    Able to capture whatever is entered in the search field, but the page refreshes immediately after the submit button is clicked.

--- src/App.js
+++ src/App.js
@@ -4,54 +4,68 @@ import './App.css';
 
 
 var movies = [
-  'Mean Girls',
-  'Hackers',
-  'The Grey',
-  'Sunshine',
-  'Ex Machina'
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
 ];
 
-// returnMovies = (movies) => {
-//   var titles = [];
-//    movies.map( (movie) => {
-//       titles.push(movie.title);
-//   });
-//   return titles;
-// }
+// document.getElementById("input-box").value
 
-var MovieListEntry = (props) => (
-  <div className="move-list-entry">
-  {props.movie}
-  </div>
-)
 
-// var MovieList = (props) => (
-//   <div>
-//   {props.movies.map((movie, index) => (
-//     <MovieListEntry movie={movie}  />
-//     )
-//     )}
-//   </div>
-//   );
+
+
+
 
 class App extends Component {
   constuctor(props) {
     // super(props);
+    this.state = {
+      value: "Search here!"
+    }
   }
+
+  handleSearchClick (video) {
+    // event.preventDefault();
+    // console.log('clicked!')
+    console.log(video);
+  };
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Movie List</h2>
+        <div>
+          <Search handleSearchClick={this.handleSearchClick.bind(this)}/>
+        </div>
         </div>
   {movies.map((movie, index) => (
-    <MovieListEntry movie={movie} key={index} />
+    <MovieListEntry movie={movie.title} key={index} />
     )
     )}
       </div>
     );
   }
 }
+
+var MovieListEntry = (props) => (
+  <div className="move-list-entry">
+  {props.movie}
+  </div>
+);
+
+var Search = (props) => (
+  <div>
+  <form>
+    <input id="input-box" ></input>
+    <button id="submit-button" 
+    onClick={() => {props.handleSearchClick(document.getElementById('input-box').value)}}
+    >Submit</button>
+  </form>
+  </div>
+);
 
 window.App = App;
 export default App;
